092691019a154d4e77e8dcee5bd02c5b49edf0b6
Helene WOLFF <hwolff@e1r13p1.42.fr>
2018-11-05 16:43:13 +0100

    suite arithmetic

--- srcs/expansions/arithmetics.c
+++ srcs/expansions/arithmetics.c
@@ -6,21 +6,23 @@
 /*   By: hwolff <hwolff@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/01 13:29:09 by hwolff            #+#    #+#             */
-/*   Updated: 2018/11/01 18:41:56 by hwolff           ###   ########.fr       */
+/*   Updated: 2018/11/05 16:38:05 by hwolff           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#include "../../includes/shell.h"
 
 int     is_arithmetics(char *data, int p)
 {
-    ft_putendl("coucou");
     if (data[p + 1] == ')')
-    {
         return (0);
-    }
     //division by zero
     return (0);
+}
+
+static int	is_operator(char c)
+{
+	return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%');
 }
 
 int 		check_if_arithmetics(char *data)
@@ -38,10 +40,13 @@ int 		check_if_arithmetics(char *data)
 	i = ft_strlen(data) - 2;
 	while (data[i--] == ')')
 		pf++;
+	i = 0;
 	if (po == pf)
 	{
-        ft_putnbr(po);
         res = ft_strsub(data, po, ft_strlen(&data[po]) - po - 1);
+		while (i++ < ft_strlen(res))
+			if (is_hexa(res[i]) == 0 && is_operator(res[i]) == 0 && res[i] != ' ' && res[i] != '\0')
+				return (0);
 		is_arithmetics(res, po);
 		return (1);
 	}
@@ -49,3 +54,15 @@ int 		check_if_arithmetics(char *data)
 		ft_putendl("Parse error");
 	return (0);
 }
+
+// int		main(int argc, char **argv, char **envp)
+// {
+// 	t_data	data;
+// 	(void)argc;
+
+// 	data.env = envp;
+// 	data.args = argv;
+	
+// 	check_if_arithmetics(data.args[1]);
+// 	return (0);
+// }
