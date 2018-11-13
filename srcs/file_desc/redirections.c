568cd95784e17a92f2602ddc8ed75b051f0d7a87
Helene WOLFF <hwolff@e1r12p7.42.fr>
2018-11-13 18:40:38 +0100

    .h norme et redirections OK

--- srcs/file_desc/redirections.c
+++ srcs/file_desc/redirections.c
@@ -6,8 +6,36 @@
 /*   By: hwolff <hwolff@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 15:08:32 by hwolff            #+#    #+#             */
-/*   Updated: 2018/11/06 15:49:51 by hwolff           ###   ########.fr       */
+/*   Updated: 2018/11/13 18:08:36 by hwolff           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#include "../../includes/shell.h"
+
+int     exec_one_redirect(t_data *data, int file[], int rafter)
+{
+    char    buf;
+    int     ret;
+    pid_t   pid;
+    
+    if (rafter == 1)
+        ret = open(data->args[2], O_CREAT|O_WRONLY, 0666);
+    else if (rafter == 2)
+        ret = open(data->args[2], O_APPEND);
+    if (ret >= 0)
+    {
+        pid = fork();
+        dup2(file[2], 1);
+        close(file[2]);
+         while (read(file[1], &buf, 1) > 0)
+            write(file[2], &buf, 1);
+        write(STDOUT_FILENO, "\n", 1);
+        is_builtins(data);
+    }
+    else
+    {
+        ft_dprintf(STDERR_FILENO, ""ERR_PREFIX"error -- %s\n");
+        exit(EXIT_FAILURE);
+    }
+    return (0);
+}
