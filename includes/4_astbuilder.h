a931d45a66fc12f9b28a81c103596bba6b84816e
Helene WOLFF <hwolff@e1r10p1.42.fr>
2018-11-28 18:13:00 +0100

    debut fct ast

--- includes/4_astbuilder.h
+++ includes/4_astbuilder.h
@@ -6,7 +6,7 @@
 /*   By: hwolff <hwolff@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 15:19:31 by hben-yah          #+#    #+#             */
-/*   Updated: 2018/11/26 16:02:34 by hwolff           ###   ########.fr       */
+/*   Updated: 2018/11/28 18:08:14 by hwolff           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 ** astbuilder.c
 */
 
-void	            build_ast(void);
+void	            build_ast(t_token *token);
+void	        add_node(t_ast **tree, int type);
 
 
 #endif
