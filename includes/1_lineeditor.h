eaf7b058ed19215c8b3115b01af3088f5755049b
Heidy BEN YAHIA <hben-yah@student.42.fr>
2018-12-05 10:58:55 +0100

    erreurs dans l'ed de ligne

--- includes/1_lineeditor.h
+++ includes/1_lineeditor.h
@@ -6,7 +6,7 @@
 /*   By: hben-yah <hben-yah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 15:26:21 by hben-yah          #+#    #+#             */
-/*   Updated: 2018/12/03 21:29:29 by hben-yah         ###   ########.fr       */
+/*   Updated: 2018/12/03 21:54:16 by hben-yah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,9 @@ void			history_dmove(t_edl *edl, char **line, t_hist *hist);
 ** command_reader.c
 */
 
-void	read_command_line(t_hist *hist);
+void	read_command_line();
 int	len_line(t_edl *edl);
-void	read_line(t_hist *hist);
+void	read_line();
 int	ft_enter(char *line, t_edl *edl);
 t_edl	*setup_edl(t_edl *edl);
 void	setup_env(int ac, char **av, t_env *env);
