40b9936fc3a4fda764f47ce29ae95ff597fb2b4e
Pauline <pauline@air-de-pauline.home>
2018-12-12 13:46:35 +0100

    ctrl+c

--- srcs/0_shell/shell.c
+++ srcs/0_shell/shell.c
@@ -45,6 +45,7 @@ void	command_line_loop(void)
 	setup_hist(&data->hist);
 	while (!data->shell_exit)
 	{
+		signal_list();
 		read_command_line(&data->hist);
 		//data->cmd_line = g_line_test;
 		//ft_printf("%s\n", data->cmd_line);
