649b9121388023c7c978f566bf9d777a7932cccc
Alexandre KARASSOULOFF <akarasso@e1r11p7.42.fr>
2018-12-16 17:00:40 +0100

    [Update libft] Add new func to handle arr/str

---
+++ libft/srcs/string/ft_3strjoinfree.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+static void	ft_3strjoinutil(char *s1, char *s2, char *s3, char flag)
+{
+	if (s1 && (flag & FREE_LEFT))
+		free(s1);
+	if (s2 && (flag & FREE_MID))
+		free(s2);
+	if (s3 && (flag & FREE_RIGHT))
+		free(s3);
+}
+
+char		*ft_3strjoinfree(char *s1, char *s2, char *s3, char flag)
+{
+	char	*new;
+	int		len;
+
+	len = (s1) ? ft_strlen(s1) : 0;
+	len += (s2) ? ft_strlen(s2) : 0;
+	len += (s3) ? ft_strlen(s3) : 0;
+	if (!(new = ft_strnew(len)))
+	{
+		ft_3strjoinutil(s1, s2, s3, flag);
+		return (0);
+	}
+	if (s1)
+		ft_strcat(new, s1);
+	if (s2)
+		ft_strcat(new, s2);
+	if (s3)
+		ft_strcat(new, s3);
+	ft_3strjoinutil(s1, s2, s3, flag);
+	return (new);
+}
